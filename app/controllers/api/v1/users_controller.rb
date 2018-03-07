407ac83c7f87520601289a01bb692dc6fa3f9246
Neha Kadam <nehakadam9@gmail.com>
2018-03-07 15:25:56 +0530

    Adds update action to the users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,9 +2,11 @@ class Api::V1::UsersController < ApplicationController
 
 	respond_to :json
 
+
   def show
     respond_with User.find(params[:id])
   end
+
 
   def create
     user = User.new(user_params)
@@ -14,6 +16,18 @@ class Api::V1::UsersController < ApplicationController
       render json: { errors: user.errors }, status: 422
     end
   end
+
+
+  def update
+	  user = User.find(params[:id])
+
+	  if user.update(user_params)
+	    render json: user, status: 200, location: [:api, user]
+	  else
+	    render json: { errors: user.errors }, status: 422
+	  end
+	end
+	
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
     # We are going to list our resources here
 
-    resources :users, :only => [:show, :create]
+    resources :users, :only => [:show, :create, :update]
 
   end
 
