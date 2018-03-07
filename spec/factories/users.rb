6e71a8237498cd96a27a1f8769dc30a1413afd82
Neha Kadam <nehakadam9@gmail.com>
2018-03-07 13:11:29 +0530

    Adds devise user model

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,9 @@
 FactoryBot.define do
 
   factory :user do
+  	email { FFaker::Internet.email }
+    password "12345678"
+    password_confirmation "12345678"
   end
-  
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
-require 'rails_helper'
+#require 'rails_helper'
+require 'spec_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+#RSpec.describe User, type: :model do
+describe User do
+  #pending "add some examples to (or delete) #{__FILE__}"
+
+  before { @user = FactoryBot.build(:user) }
+
+  subject { @user }
+
+  it { should respond_to(:email) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+
+  it { should be_valid }
+
 end
